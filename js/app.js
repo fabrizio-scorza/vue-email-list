5452bc1ef5d1eabd46a2e92d6eade52557ae3be3
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-04-08 17:36:28 +0200

    FIX - created

--- js/app.js
+++ js/app.js
@@ -3,23 +3,23 @@ const { createApp } = Vue
 createApp({
     data() {
         return{
-            emails:[]
+            emails:[],
         }
     },
     methods: {
         //metodo che genera la mail random dall'API
-        randomMail() {
-            axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((mailApi) =>{
-                console.log(mailApi.data.response)
-                this.emails.push(mailApi.data.response)
-            })
+        randomMails() {
+            for (i = 0; i < 10; i++){  
+                axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((mailApi) =>{
+                    console.log(mailApi.data.response)
+                    this.emails.push(mailApi.data.response)
+                })
+            }
         }
     },
-    created(){
-        for (i = 0; i < 10; i++){       
-        this.randomMail()
-        }
+    created(){             
+        this.randomMails()        
     }
 }).mount('#app')
